8e6ac6cf868f7dcfca7b14d92413db1b90d5129d
Alejandro restrepo <andresgomez@MacBook-Pro-de-Andres.local>
2024-09-25 14:58:11 -0500

    feat: se agrega los testimonios en un archivo diferente para no quemar esa informacion en el componente directamente si no manejarlo de una forma mas dinamica.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import './App.css';
 import Testimonio from './componentes/Testimonio';
+import testimonios from './testimonios';
 
 function App() {
   return (
     <div className='App'>
       <div className='contenedor-principal'>
         <h1>El poder del software para transformar vidas</h1>
-        <Testimonio
-          nombre='Jhon King'
-          pais='Singapur'
-          imagen='shawn'
-          cargo='Ingeniero de Software'
-          empresa='Amazon'
-          testimonio='Aprender a programar cambió mi perspectiva. Lo que empezó como curiosidad se convirtió en mi nueva vocación. Gracias a horas de dedicación y práctica, encontré en el desarrollo de software una forma de crecer profesionalmente y conseguir un trabajo gratificante en la industria tecnológica.'/>
-        <Testimonio
-          nombre='Laura Chima'
-          pais='Nigeria'
-          imagen='sarah'
-          cargo='Ingeniera de Software'
-          empresa='ChatDesk'
-          testimonio='El camino del software me mostró una nueva realidad. Comenzar desde cero es intimidante, pero la perseverancia en el aprendizaje me llevó a descubrir mi capacidad para construir soluciones tecnológicas. Hoy, como desarrollador, disfruto de una carrera dinámica y desafiante.'/>
-        <Testimonio 
-          nombre='Carlota Greendly'
-          pais='Suecia'
-          imagen='emma'
-          cargo='Ingeniera de Software'
-          empresa='Spotify'
-          testimonio='La programación abre infinitas puertas. Invertir tiempo en aprender a desarrollar software no solo te brinda nuevas habilidades, sino que también te permite resolver problemas de maneras innovadoras. En menos de un año, descubrí mi pasión por el código y logré transformar mi carrera en una con oportunidades ilimitadas.'/>
+        {testimonios.map((testimonio, index) => (
+                <Testimonio
+                    key={index}
+                    nombre={testimonio.nombre}
+                    pais={testimonio.pais}
+                    imagen={testimonio.imagen}
+                    cargo={testimonio.cargo}
+                    empresa={testimonio.empresa}
+                    testimonio={testimonio.testimonio}
+                />
+            ))}
       </div>
     </div>
   );
---
+++ src/testimonios.js
@@ -0,0 +1,29 @@
+const testimonios = [
+    {
+        nombre: 'Jhon King',
+        pais: 'China',
+        imagen: 'shawn',
+        cargo: 'Ingeniero de Software',
+        empresa: 'Amazon',
+        testimonio: 'Aprender a programar cambió mi perspectiva. Lo que empezó como curiosidad se convirtió en mi nueva vocación. Gracias a horas de dedicación y práctica, encontré en el desarrollo de software una forma de crecer profesionalmente y conseguir un trabajo gratificante en la industria tecnológica.',
+    },
+    {
+        nombre: 'Emma Johnson',
+        pais: 'EE.UU.',
+        imagen: 'sarah',
+        cargo: 'Desarrolladora Frontend',
+        empresa: 'Google',
+        testimonio: 'El desarrollo de software es una carrera llena de posibilidades. Tras muchas horas de aprendizaje autodidacta, descubrí que programar me permitió crear, innovar y avanzar en mi carrera. En menos de un año, pasé de ser principiante a profesional en el campo de la tecnología.',
+    },
+    {
+        nombre: 'Carlota Greendly',
+        pais: 'Alemania',
+        imagen: 'emma',
+        cargo: 'Desarrollador Backend',
+        empresa: 'Facebook',
+        testimonio: 'El camino del software me mostró una nueva realidad. Comenzar desde cero es intimidante, pero la perseverancia en el aprendizaje me llevó a descubrir mi capacidad para construir soluciones tecnológicas. Hoy, como desarrollador, disfruto de una carrera dinámica y desafiante',
+    },
+    
+];
+
+export default testimonios;
